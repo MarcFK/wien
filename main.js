8c489a8a2b9692d2239ef9644af6a4a745fe789d
MarcFK <marc.kaufer@student.uibk.ac.at>
2024-04-18 17:21:22 +0200

    overlay sehenswürdigkeiten

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 12);
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
+let themaLayer = {
+  sights: L.featureGroup().addTo(map)
+}
+
 // Hintergrundlayer
 L.control
   .layers({
@@ -25,6 +29,8 @@ L.control
     "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
     "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
     "OpenSnowMap": L.tileLayer.provider("OpenSnowMap.pistes"),
+  }, {
+    "Sehenswürdigkeiten": themaLayer.sights,
   })
   .addTo(map);
 
@@ -57,7 +63,7 @@ async function loadSights(url) {
   let response = await fetch(url);
   let geojson = await response.json();
   console.log(geojson); // nicht unbedingt nötig
-  L.geoJSON(geojson).addTo(map);
+  L.geoJSON(geojson).addTo(themaLayer.sights);
 }
 
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
