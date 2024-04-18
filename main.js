a79ef4cf2cceb98e1149d47cb434be4aaa617636
MarcFK <marc.kaufer@student.uibk.ac.at>
2024-04-18 16:11:50 +0200

    layer control

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ L.control
     "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
     "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
     "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
+    "OpenSnowMap": L.tileLayer.provider("OpenSnowMap.pistes"),
   })
   .addTo(map);
 
